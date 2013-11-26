b15bbf2071966a5fffd63aea1dd446f116bd43d5
mcsoto <esotopia@hotmail.com>
2013-11-26 00:12:10 -0200

    replaced 'frame' by 'package'; frame class replaced by list class

--- logic.js
+++ logic.js
@@ -5,20 +5,24 @@ function assert(x,str) {
 	if (x===false) throw new Error(str)
 }
 
-var logic = {}
-
-	
-/*
-	frames
-*/
-
-function Frame(a,b) {
-	this.first = a
-	this.rest = b
-	this.type = 'frame'
+inherit = function (P,Q){
+	P.prototype = new Q()
+	P.prototype.constructor = P
 }
 
-Frame.prototype = {
+var logic = {}
+
+/*
+	lists
+*/
+
+function List(a,b) {
+	this.first = a
+	this.rest = b
+	this.type = 'list'
+}
+
+List.prototype = {
 	is_empty : function () {
 		var s = this
 		return typeof s.rest === 'undefined'
@@ -28,14 +32,14 @@ Frame.prototype = {
 		if (s1.is_empty()) 
 			return s2
 		else 
-			return new Frame(s1.first, s1.append(s2));
+			return logic.make_list(s1.first, s1.append(s2));
 	}
 	,interleave : function (s2) {
 		var s1 = this
 		if (s1.is_empty())
 			return s2
 		else
-			return logic.make_frame(s1.first, s2.interleave(s1.rest));
+			return logic.make_list(s1.first, s2.interleave(s1.rest));
 	}
 	,forEach : function (f) {
 		var s = this
@@ -60,17 +64,17 @@ Frame.prototype = {
 			return EMPTY_STREAM
 		else {
 			var _rest = s.rest.map(f)
-			return logic.make_frame(f(s.first), _rest);
+			return logic.make_list(f(s.first), _rest);
 		}
 	 }
 	,extend : function (val) {
-		return logic.make_frame(val, this)
+		return logic.make_list(val, this)
 	}
 	,toString : function() {
 		var str = '|'
 		var s=this
 		while(!s.is_empty()) {
-			str += s.first.toString()+(s.rest.is_empty()?'':', ')
+			str += s.first.toString()+(s.rest.is_empty()?'':'; ')
 			s=s.rest
 		}
 		str+='|'
@@ -79,17 +83,55 @@ Frame.prototype = {
 	,write : function () {
 		write(this.toString())
 	}
-	,lookup_binding_helper : function (frame, variable) {
-		if(frame===EMPTY_FRAME) return false
-		else if(frame.first.variable===variable) return frame.first
-		else return frame.rest.lookup_binding(variable)
-	}
-	,lookup_binding : function (variable) {
-		return this.lookup_binding_helper(this, variable)
-	}
 }
 
-/* 
+
+/*
+	packages hold a **frame** and a **constraint store**
+*/
+
+function Package(f, cs) {
+	this.frame = f
+	this.store = cs
+	this.type = 'package'
+}
+
+Package.prototype.lookup_binding_helper = function (frame, variable) {
+	if(frame===EMPTY_LIST) return false
+	else if(frame.first.variable===variable) return frame.first
+	else return this.lookup_binding_helper(frame.rest, variable)
+}
+
+Package.prototype.lookup_binding = function (variable) {
+	return this.lookup_binding_helper(this.frame, variable)
+}
+
+Package.prototype.is_empty = function (arguments) {
+	return this.frame.is_empty(arguments)
+}
+
+Package.prototype.extend_frame = function (f) {
+	return logic.make_package(f, this.store)
+}
+
+Package.prototype.extend_store = function (cs) {
+	return logic.make_package(this.frame, cs)
+}
+
+Package.prototype.extend = function (arguments) {
+	return logic.make_package(this.frame.extend(arguments), this.store)
+}
+
+Package.prototype.toString = function() {
+	var str = '{'+this.frame.toString()+', '+this.store+'}'
+	return str
+}
+
+Package.prototype.write = function () {
+	write(this.toString())
+}
+
+/*
 	streams
 */
 
@@ -204,26 +246,35 @@ Binding.prototype.toString = function () { return (typeof this.variable.name!=='
 */
 
 var EMPTY_STREAM = new Stream(undefined, undefined)
-var EMPTY_FRAME = new Frame(undefined, undefined)
+var EMPTY_LIST = new List(undefined, undefined)
+var EMPTY_PACKAGE = new Package(EMPTY_LIST, EMPTY_LIST)
 
-logic.nil = EMPTY_FRAME
+logic.nil = EMPTY_PACKAGE //a goal needs a package; therefore it initially receives 'nil' (the first package)
 logic.EMPTY_STREAM = EMPTY_STREAM
-logic.EMPTY_FRAME = EMPTY_FRAME
+logic.EMPTY_LIST = EMPTY_LIST
 
-logic.is_stream = function (s) {
-	return typeof s==='object' && s.type && s.type==='stream'
+function has_type(obj, type) {
+	return (typeof obj==='object') && (typeof obj.type!=='undefined') && obj.type===type
 }
 
-logic.is_frame = function (s) {
-	return typeof s==='object' && s.type && s.type==='stream'
+logic.is_stream = function (v) {
+	return has_type(v, 'stream')
 }
 
-logic.is_binding = function (s) {
-	return typeof s==='object' && s.type && s.type==='binding'
+logic.is_list = function (v) {
+	return has_type(v, 'list')
+}
+
+logic.is_package = function (v) {
+	return has_type(v, 'package')
+}
+
+logic.is_binding = function (v) {
+	return has_type(v, 'binding')
 }
 
 logic.is_lvar = function (v) {
-	return (typeof v==='object') && (typeof v.type!=='undefined') && v.type==='lvar'
+	return has_type(v, 'lvar')
 }
 
 logic.is_array = function (v) {
@@ -249,12 +300,16 @@ logic.make_stream = function (a,b) {
 	return new Stream(a,b)
 }
 
-logic.make_frame = function (a,b) {
+logic.make_list = function (a,b) {
 	if(typeof b==='undefined') b = EMPTY_LIST
 	assert(typeof b==='object', '#2 argument must be object.')
-	return new Frame(a,b)
+	return new List(a,b)
 }
 
+logic.make_package = function (f, cs) {
+	assert(logic.is_list(f) && logic.is_list(cs), "#1 and #2 arguments of package must be list")
+	return new Package(f,cs)
+}
 
 logic.unify = function (a, b, frame) {
 	if(frame===false) 
@@ -318,19 +373,22 @@ function walk(variable, frame) {
 logic.walk = walk
 
 /*
-	a goal/relation takes a frame and returns a stream of frames (an empty stream stands for a 'fail'/'false' goal)
+	a goal/relation takes a package and returns a stream of packages (an empty stream stands for a 'fail'/'false' goal)
 */
 
 logic.eq = function (a, b) { //'goal' version of unify
-	return function(f) {
+	return function(p) {
+		var f = p.frame
 		var f2 = logic.unify(a, b, f)
-		if(f2) return logic.win(f2)
-		else return logic.fail()
+		if(f2) 
+			return logic.win(p.extend_frame(f2))
+		else 
+			return logic.fail()
 	}
 }
 
-logic.win = function (frame) { return logic.make_stream(frame||logic.nil) }
-logic.fail = function (frame) { return EMPTY_STREAM }
+logic.win = function (pack) { return logic.make_stream(pack||logic.nil) }
+logic.fail = function (pack) { return EMPTY_STREAM }
 
 /*
 	some functions that operate on goals
@@ -357,7 +415,6 @@ logic.and = logic.conj
 	non-fundamental goals
 */
 
-
 var _between = function (a,b,x) {
 	if(a>b) 
 		return logic.fail
@@ -371,7 +428,7 @@ logic.between = function (a,b,x) {
 	return _between(a,b,x)
 }
 
-logic.run = function (g, v, n) { 
+logic.run = function (g, v, n) {
 	//runs goal getting the first n results of variables
 	//n is optional (n=n or infinity)
 	//v is variable or array of variables
@@ -380,17 +437,18 @@ logic.run = function (g, v, n) {
 	var s = g(logic.nil)
 		,result = []
 	for(var i=0; i<n && !s.is_empty();++i) {
-		var frame = s.first
+		var pack = s.first
+			,frame = pack.frame
 		if(logic.is_lvar(v)) { //get variable into result
 			var v2 = walk(v, frame)
-			var _temp = logic.is_lvar(v2) ? frame.lookup_binding(v).val : v2
+			var _temp = logic.is_lvar(v2) ? pack.lookup_binding(v).val : v2
 			result.push(_temp)
 		}
 		else { //get array of variables into result
 			var vals = []
 			for(var j=0;j<v.length;++j) {
 				var v2 = walk(v[j], frame)
-				var _temp = logic.is_lvar(v2) ? frame.lookup_binding(v2).val : v2
+				var _temp = logic.is_lvar(v2) ? pack.lookup_binding(v2).val : v2
 				vals.push(_temp)
 			}
 			result.push(vals)
